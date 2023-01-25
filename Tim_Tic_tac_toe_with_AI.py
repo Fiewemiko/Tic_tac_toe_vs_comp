7fcb4ac4068a5ece07b873959b49a1a1c94c5595
Fiewemiko <filipniewczas12@gmail.com>
2023-01-25 15:22:45 +0100

    initial commit

---
+++ Tim_Tic_tac_toe_with_AI.py
@@ -0,0 +1,137 @@
+board = [' ' for _ in range(10)]
+
+
+def insertLetter(letter, pos):
+    board[pos] = letter
+
+
+def SpaceIsFree(pos):
+    return board[pos] == ' '
+
+
+def printBoard(board):
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + ' \n'
+                                                                 '-----------\n'
+                                                                 ' ' + board[4] + ' | ' + board[5] + ' | ' + board[
+              6] + ' \n'
+                   '-----------\n'
+                   ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + ' \n')
+
+
+def isWinner(bo, le):
+    return (
+            (bo[7] == le and bo[8] == le and bo[9] == le)
+            or (bo[4] == le and bo[5] == le and bo[6] == le)
+            or bo[1] == le and bo[2] == le and bo[3] == le
+            or bo[1] == le and bo[5] == le and bo[9] == le
+            or bo[3] == le and bo[5] == le and bo[7] == le
+            or bo[3] == le and bo[9] == le and bo[6] == le
+            or bo[1] == le and bo[4] == le and bo[7] == le
+            or bo[2] == le and bo[5] == le and bo[8] == le
+    )
+
+
+def playerMove():
+    run = True
+    while run:
+        move = input('please select a position to place an \'X\' (1-9): ')
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if SpaceIsFree(move):
+                    run = False
+                    insertLetter('X', move)
+                else:
+                    print('Sorry, this space is occupied!')
+            else:
+                print('Please type a number within the range!')
+        except:
+            print('Please type a number!')
+
+
+def compMove():
+    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
+    move = 0  # wtedy kiedy nie ma żadnego ruchu już spełniających below statementy
+    for let in ['O', 'X']:
+        for i in possibleMoves:
+            boardCopy = board[:]
+            boardCopy[i] = let
+            if isWinner(boardCopy, let):
+                move = i
+                return move
+
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1, 3, 7, 9]:
+            cornersOpen.append(i)
+
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2, 4, 6, 8]:
+            edgesOpen.append(i)
+
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+
+    if isBoardFull(board):  # przerzuć do komputer move
+        print('Tie Game')
+
+    return move
+
+
+def selectRandom(li):
+    import random
+    ln = len(li)
+    r = random.randrange(0, ln)
+    return li[r]
+
+
+def isBoardFull(board):
+    if board.count(' ') > 1:
+        return False
+    else:
+        return True
+
+
+def main():
+    print("Welcome to Tic Tac Toe! \n")
+    printBoard(board)
+
+    while not (isBoardFull(board)):
+        if not (isWinner(board, 'O')):
+            playerMove()
+            printBoard(board)
+        else:
+            print('Sorry, O\'s won this time! ')
+            break
+
+        if not (isWinner(board, 'X')):
+            move = compMove()
+            if move == 0:
+                print('')
+            else:
+                insertLetter('O', move)
+                print('Computer placed an \'O\' in position', move, ':')
+                printBoard(board)
+        else:
+            print('X\'s won this time! Good Job! ')
+            break
+
+
+while True:
+    main()
+    a = input('Play again (y/n)?: ')
+    if a.lower() == 'y':
+        board = [' ' for _ in range(10)]
+        main()
+    else:
+        print('Goodbye! ')
+        break
